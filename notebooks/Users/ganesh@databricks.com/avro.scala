f05d4c5a4284ade6d62c5f851b28de0bbb116cda
Ganesh Chand <ganeshchand@gmail.com>
2016-08-15 13:25:06 -0700

    ??????????????

---
+++ notebooks/Users/ganesh@databricks.com/avro.scala
@@ -0,0 +1,12 @@
+// Databricks notebook source exported at Mon, 15 Aug 2016 20:25:05 UTC
+import org.apache.avro.Schema
+
+// COMMAND ----------
+
+import com.databricks.spark.avro._
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC 
+// MAGIC This is a test
